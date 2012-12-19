7bd622bf76e2ae5d53eff9ebe1d2cf42028420ab
Francesco Biscani <bluescarni@gmail.com>
2012-12-19 12:09:57 +0100

    Improvements and fixes in the master agent (better error generation/handling, remove uselss check on urls()).

--- master.py
+++ master.py
@@ -10,30 +10,23 @@ class agent(object):
 		self.__agent_list = []
 		self.__lock = Lock()
 		super().__init__(**kwargs)
-	def __wrap_url_request(self,agent):
-		try:
-			return agent.urls()
-		except:
-			return None
 	@_rpc.enable_rpc
 	def spawn(self,t,*args,**kwargs):
-		# TODO:
-		# - better granularity at error handling (check separately existence of agent type and construction of agent -> but maybe this
-		#   is already handled by the RPC system, will return a string with the error.
-		# - allow for import of agents outside the jezebel module.
 		import importlib
 		if not isinstance(t,str):
 			raise TypeError('agent type must be a string')
 		try:
-			m = importlib.import_module('..' + t,'jezebel.master')
 			self.__logger.info('attempting to spawn an agent of type "' + t + '"')
-			with self.__lock:
-				self.__agent_list.append(m.agent(*args,**kwargs))
-				retval = self.__wrap_url_request(__agent_list[-1])
-			return retval
-		except (ImportError, AttributeError):
-			raise TypeError('invalid agent type')
+			m = importlib.import_module('..' + t,'jezebel.master')
+			a_type = m.agent
+		except (ImportError,AttributeError):
+			raise TypeError('no agent type "' + t + '" found')
+		new_agent = a_type(*args,**kwargs)
+		retval = new_agent.urls()
+		with self.__lock:
+			self.__agent_list.append(new_agent)
+		return retval
 	@_rpc.enable_rpc
 	def agent_urls(self):
 		with self.__lock:
-			return [self.__wrap_url_request(a) for a in self.__agent_list]
+			return [a.urls() for a in self.__agent_list]
