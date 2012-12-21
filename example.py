e1d3cdbdd16799ab3d867f069a1b06966c5029a5
Francesco Biscani <bluescarni@gmail.com>
2012-12-21 11:31:58 +0100

    example: add wait() method for testing.

--- example.py
+++ example.py
@@ -6,3 +6,7 @@ class agent(_xmpp.agent,_directions.agent,_master.agent,_http.agent,_rpc.agent):
 		self.__logger = logging.getLogger('jezebel.example.agent')
 		self.__logger.info('initialising example agent')
 		super().__init__(**kwargs)
+	@_rpc.enable_rpc
+	def wait(self):
+		from time import sleep
+		sleep(60)
